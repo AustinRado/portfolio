2af6538a835e46b5dab201b3d1eb26209dfee801
AustinRado <radoaussy@gmail.com>
2023-12-22 22:35:21 +0300

    Hero section changes

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,26 +9,12 @@ const HeroSection = () => {
     <section> 
       <div className='flex justify-center mt-9 pt-[40px]'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
-            <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600'>Hello, I am {''}</span>
+            <h1 className="text-white mb-4 text-2xl sm:text-5xl lg:text-4xl font-extrabold">
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600'>Welcome, My name is Austin Rado</span>
               <br/>
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed out once, initially
-                  'Austin Rado',
-                  2000, // wait 1s before replacing "Mice" with "Hamsters"
-                  'a react developer',
-                  1500,
-                  'a software engineer',
-                  1500,
-                ]}
-                wrapper="span"
-                speed={50}
-                repeat={Infinity}
-              />
             </h1>
             <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-            lorem ipsum dolor sit amet consectetur adipisicing elit. Quisud voluptatum.
+              I am a <span>full stack developer</span> with 3 years of experience. I enjoy building applications, sites and good relationships. My focus is React(Next.js), NodeJs, AWS and Python (Flusk, Django). I am always open to new experiences as well as opportunities and collaboration.
             </p>
             <div>
                 <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Socials</button>

8d00665cb70b4857c668085166690d91e1cd3251
AustinRado <radoaussy@gmail.com>
2023-12-23 15:24:29 +0300

    social media links

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-center">
-        <p className="text-slate-600">All rights reserved.</p>
+        <p className="text-slate-600">All rights reserved. {year}</p>
       </div>
     </footer>
   );
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,7 +1,8 @@
 "use client"
 
 import React from 'react'
-
+import { socialLinks } from '../lib/data';
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
@@ -15,11 +16,16 @@ const HeroSection = () => {
             <p className='text-white text-base sm:text-lg mb-6 lg:text-xl text-center'>
               I am a <span>full stack developer</span> with 3 years of experience. I enjoy problem-solving, building applications and websites. My focus is React(Next.js), NodeJs, APIs, AWS and Python (Flusk, Django). I am always open to new experiences.
             </p>
-            <div className='text-center'>
+            <div className='text-center flex flex-row items-baseline justify-center'>
+                <div>
                 <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Contact me</button>
                 <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
                   <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Resume</span>
                 </button>
+                </div>
+                <div className='flex items-center  gap-3 mt-4'>
+                Github...LinkedIn...Twitter
+                </div>
             </div>
         </div>
       </div>

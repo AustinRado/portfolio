2c074dec2ecc73b85ad07fb9cc6cbbc0a858d332
AustinRado <radoaussy@gmail.com>
2024-01-03 16:50:35 +0300

    added links to contacts

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 //   
 import Link from "next/link";
 import Image from "next/image";
+import { socialLinks } from "../lib/data";
 
 
 const Contact = () => {
@@ -29,23 +30,18 @@ const Contact = () => {
         <div className="grid md:grid-cols-2 my-12 md:my-12 gap-4 relative">
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">
-            Let&apos;s Connect
-        </h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-            {" "}
-            I&apos;m open to new opportunities, my inbox is always
-            active. Whether you have a question or just want to say hi, I&apos;ll
-            try my best to get back to you!
-        </p>
-        <div className="socials flex flex-row gap-2">
-            <Link href="github.com">
-            <Image src='' alt="Github Icon" />
-            </Link> 
-            <Link href="linkedin.com">
-            <Image src='' alt="Linkedin Icon" />
-            </Link>
-        </div>
+            <h5 className="text-xl font-bold text-white my-2">
+                Let&apos;s Connect
+            </h5>
+            <p className="text-[#ADB7BE] mb-4 max-w-md">
+                {" "}
+                I&apos;m open to new opportunities, my inbox is always
+                active. Whether you have a question or just want to say hi, I&apos;ll
+                try my best to get back to you!
+            </p>
+            <div className='flex items-center gap-3 mt-4'>
+                      {socialLinks.map((link, index)=><Link href={`${link.path}`} key={index}>{link.display}</Link>)}
+            </div>
         </div>
         <div>
         {emailSubmitted ? (
--- app/lib/data.js
+++ app/lib/data.js
@@ -1,3 +1,7 @@
+import { AiFillGithub, AiFillTwitterCircle } from 'react-icons/ai';
+import { RiLinkedinFill } from 'react-icons/ri';
+
+
 export const skillsData = [
     "HTML",
     "CSS",
@@ -25,4 +29,19 @@ export const skillsData = [
     "Postman",
     "AWS",
     "Framer Motion",
+  ]
+
+  export const socialLinks = [
+    {
+      path: "https://github.com/AustinRado/",
+      display:<AiFillGithub className='group-hover:text-white w-7 h-7'/>,
+    },
+    {
+      path: "https://twitter.com/omburo_",
+      display: <AiFillTwitterCircle className='group-hover:text-white w-7 h-7' />,
+    },
+    {
+      path: "https://www.linkedin.com/in/austin-omburo-527919268/",
+      display: <RiLinkedinFill className='group-hover:text-white w-7 h-7' />,
+    },
   ]

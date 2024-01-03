002159b9900c8027cc05ba3a3e9d5c1359ba4ed7
AustinRado <radoaussy@gmail.com>
2024-01-03 16:59:43 +0300

    changes to the form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -26,14 +26,14 @@ const Contact = () => {
     };
   return (
     <section>
-        <h1 className="text-2xl text-white mt-[30px] text-center relative">Contact</h1>
+        <h1 className="text-2xl text-[#030303] mt-[30px] text-center relative">Contact</h1>
         <div className="grid md:grid-cols-2 my-12 md:my-12 gap-4 relative">
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className="z-10">
-            <h5 className="text-xl font-bold text-white my-2">
+            <h5 className="text-xl font-bold text-[#030303] my-2">
                 Let&apos;s Connect
             </h5>
-            <p className="text-[#ADB7BE] mb-4 max-w-md">
+            <p className="text-[#030303] mb-4 max-w-md">
                 {" "}
                 I&apos;m open to new opportunities, my inbox is always
                 active. Whether you have a question or just want to say hi, I&apos;ll
@@ -53,7 +53,7 @@ const Contact = () => {
             <div className="mb-6">
               <label
                 htmlFor="email"
-                className="text-white block mb-2 text-sm font-medium"
+                className="text-[#030303] block mb-2 text-sm font-medium"
               >
                 Your email
               </label>
@@ -62,14 +62,14 @@ const Contact = () => {
                 type="email"
                 id="email"
                 required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                className="border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Email"
               />
             </div>
             <div className="mb-6">
               <label
                 htmlFor="subject"
-                className="text-white block text-sm mb-2 font-medium"
+                className="text-[#030303] block text-sm mb-2 font-medium"
               >
                 Subject
               </label>
@@ -78,14 +78,14 @@ const Contact = () => {
                 type="text"
                 id="subject"
                 required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                className="border border-[#33353F] placeholder-[#9CA2A9] text-black text-sm rounded-lg block w-full p-2.5"
                 placeholder="Subject "
               />
             </div>
             <div className="mb-6">
               <label
                 htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
+                className="text-[#030303] block text-sm mb-2 font-medium"
               >
                 Message
               </label>
@@ -93,13 +93,13 @@ const Contact = () => {
 
               name="message"
               id="message"
-              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              className="border border-[#070707] placeholder-[#9CA2A9] text-black text-sm rounded-lg block w-full p-2.5"
               placeholder="Let's talk about..."
             />
           </div>
           <button
             type="submit"
-            className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            className="bg-blue-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
           >
             Send Message
           </button>

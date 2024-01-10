27f458075bf0a11e08fc7e941625818c6971be9f
AustinRado <radoaussy@gmail.com>
2024-01-10 21:49:26 +0300

    changed hero

--- app/page.js
+++ app/page.js
@@ -1,14 +1,14 @@
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
-import NavBar from './components/NavBar'
-import About from './components/About'
-import Footer from './components/Footer'
-import Contact from './components/Contact'
-import Skills from './components/Skills'
+import Image from 'next/image';
+import HeroSection from './components/HeroSection';
+import NavBar from './components/NavBar';
+import About from './components/About';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col" >
+    <main className="flex min-h-screen flex-col items-center" >
       <NavBar/>
       <div className='container mx-auto px-12 py-4'>
       <HeroSection />

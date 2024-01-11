3ff6aee0681008dcb85b76d853c3150573048655
AustinRado <radoaussy@gmail.com>
2024-01-11 20:37:54 +0300

    projects section

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import { projects } from '../lib/data';
 
 const Projects = () => {
   return (
@@ -6,7 +8,18 @@ const Projects = () => {
     <h1 className="text-2xl text-[#030303] mt-[30px] text-center relative">Projects
         <hr className='w-8 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
     </h1>
-
+    <div className='flex flex-col space-y-20'>
+        {projects.map((idx, link) => {
+            return (
+                <div key={idx}>
+                    <div className='flex flex-col md:flex-row md:space-x-12'>
+                        <div className='md:w-1/2'>
+                            <Image/>
+                        </div>
+                        <div></div>
+                    </div>
+                </div>) })}
+    </div>
     </section>
   )
 }

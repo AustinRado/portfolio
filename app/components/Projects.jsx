92f3a1a765f135b0dd2078ad8410c96bb036a580
AustinRado <radoaussy@gmail.com>
2024-01-11 23:30:30 +0300

    tags added

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Image from "next/image";
+import Link from "next/link";
+import { AiFillGithub, AiFillTwitterCircle } from 'react-icons/ai';
 
 const projects = [
     {
       name:"Medicare",
-      description:"Medical booking application",
+      description:"Medicare is a web app that helps you find a doctor and book an appointment depending on services and location.",
       image:"/medicare.png",
+      tags: ["React", "Mongodb", "Express", "Tailwind", "Framer"],
       github:"https://github.com/AustinRado/medicare",
       link:"",
     },
@@ -13,6 +16,7 @@ const projects = [
       name:"Portfolio",
       description:"Personal portfolio website",
       image:"/portfolio.png",
+      tags: ["React", "Next.js", "Tailwind", "Framer"],
       github:"https://github.com/AustinRado/portfolio",
       link:"",
     },
@@ -20,6 +24,7 @@ const projects = [
       name:"",
       description:"",
       image:"",
+      tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
       github:"",
       link:"",
     },
@@ -46,8 +51,34 @@ const Projects = () => {
                             />
                         </div>
                         <div className='mt-8 md:w-1/2'>
-                                <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
-                                <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400'>{project.description}</p>
+                            <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
+                            <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400'>{project.description}</p>
+                            
+                            {/* Displaying tags */}
+                            <div className='flex flex-row align-bottom space-x-2 mb-4'>
+                            {project.tags.map((tag, tagIdx) => (
+                            <span key={tagIdx} className='text-sm bg-gray-200 dark:bg-gray-800 text-gray-700 dark:text-gray-300 px-2 py-1 rounded'>
+                            {tag}
+                            </span>
+                            ))}
+                            </div>
+
+
+                            <div className='flex flex-row align-bottom space-x-4'>
+                               <Link href={project.github} target="_blank">
+                                <AiFillGithub
+                                  size={30}
+                                  className="hover:-translate-y-1 transition-transform cursor-pointer"
+                                />
+                               </Link>
+                               <Link href={project.link} target="_blank">
+                                <AiFillTwitterCircle
+                                  size={30}
+                                  className="hover:-translate-y-1 transition-transform cursor-pointer"
+                                />
+                               </Link>
+                            </div>
+                        
                         </div>
                         
                     </div>

2ddfb3935d03314313d2941e20c8ee0193380247
AustinRado <radoaussy@gmail.com>
2024-03-03 17:45:16 +0300

    removed a button

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -15,14 +15,13 @@ const HeroSection = () => {
               <br/>
             </h1>
             <p className='text-xs sm:text-sm mb-6 lg:text-xl text-center'>
-              I am a <span className='font-bold'>full stack developer</span> with a passion for problem-solving and building responsive websites. I have experience in Linux, React(Next.js), Flask and Django frameworks, NodeJs, APIs and AWS. I am always open to collaborations and teamwork.
+              I am a full stack developer with a passion for problem-solving and finding solutions. My forte is Linux, React(Next.js), NodeJs, and APIs.
+              <br/> Currently, I am diving into cloud computing, AWS, and exploring the endless opportunities they offer for scalable and efficient solutions
+              <br/> I am always open to collaborations and teamwork.
             </p>
             <div className='text-center sm:flex sm:justify-center sm:items-baseline gap-3'>
                 <div>
-                  <button className='text-base px-1 py-1 w-full sm:w-fit rounded-full mr-2 mb-2 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black'>
-                  <span className='block  bg-slate-200 rounded-full px-4 py-2'>Contact Me</span>
-                  </button>
-                  <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-black'>
+                  <button className='px-1 py-1 w-full sm:w-fit rounded-fulls hover:bg-slate-200 text-black'>
                     <span className='block bg-slate-200 rounded-full px-4 py-2'>Resume</span>
                   </button>
                 </div>
